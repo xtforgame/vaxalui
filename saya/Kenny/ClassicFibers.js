0c5bbbc3da8a815fc3804ee765971c28ef74db39
RickChen <xtforgame@gmail.com>
2020-08-20 14:25:35 +0800

    use @material-ui/core/styles

--- saya/Kenny/ClassicFibers.js
+++ saya/Kenny/ClassicFibers.js
@@ -7,7 +7,7 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _styles = require("@material-ui/styles");
+var _styles = require("@material-ui/core/styles");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
